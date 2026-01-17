be6812ff72efc875bfc826d00d01bf30919693e0
firman <firmanwahyudialakmal@gmail.com>
2026-01-17 21:04:05 +0700

    baru buat model

---
+++ app/Models/PengajarKelas.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PengajarKelas extends Model
+{
+    /** @use HasFactory<\Database\Factories\PengajarKelasFactory> */
+    use HasFactory;
+}
